c017b5c392e5d7c88b0703cdc3f9a5ae0c9c57fc
Kazeem Ibrahim <ibrahimsen186059@futa.edu.ng>
2023-08-07 08:56:46 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Kazeem Ibrahim",
+  title: "Product Designer",
+  email: "kazeemibrahim31@gmail.com",
+  gitHub: "caxrii",
+  instagram: "caxri__",
+  linkedIn: "caxri",
+  medium: "caxri__",
+  twitter: "caxri__",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
